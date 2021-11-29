c5f02b67377bb7e3609280b984b4c760419238eb
haa48 <haa48@blu9402u-d05.csil.sfu.ca>
2021-11-29 03:36:24 -0800

    initial

--- mixer/train_mixer.py
+++ mixer/train_mixer.py
@@ -2,6 +2,8 @@ import os
 import glob
 import numpy as np
 
+from torch.utils.data import Dataset, DataLoader
+
 from dataset import PartNetDataset
 
 
@@ -9,9 +11,18 @@ from dataset import PartNetDataset
 # Setup Data
 
 dataset = PartNetDataset(phase='train',data_root='/scratch/data_v0/')
-
+dataloader = DataLoader(dataset, batch_size=16, shuffle=True, num_workers=4,
+                            worker_init_fn=np.random.seed())
 # Setup Network
+
+#encoder (MLP), Generator(transformations), Discriminator
 
 
 
 # Train
+
+
+for b, data in enumerate(dataloader):
+    print(data)
+
+    break
